6914f55a533adeec2f79005e3a098f71b220ad2c
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-02 22:21:51 +0300

    Use viewModels extension to init DaysListViewModel

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/dayslist/DaysListFragment.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/dayslist/DaysListFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.RecyclerView
 import dev.kirillzhelt.maymaymay.MainApplication
@@ -16,7 +17,7 @@ import dev.kirillzhelt.maymaymay.R
  */
 class DaysListFragment : Fragment() {
 
-    private lateinit var daysListViewModel: DaysListViewModel
+    private val daysListViewModel: DaysListViewModel by viewModels { DaysListViewModelFactory(MainApplication.daysRepository) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,12 +26,8 @@ class DaysListFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_days_list, container, false)
 
-        daysListViewModel = DaysListViewModel(MainApplication.daysRepository)
-
         val daysListRecyclerView: RecyclerView = view.findViewById(R.id.fragment_days_list_rv)
-
         val daysListAdapter = DaysListAdapter()
-
         daysListRecyclerView.adapter = daysListAdapter
 
         daysListViewModel.days.observe(this, Observer { days ->
