7712236d5a664761795524d6fc90d7caca6d057e
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-05 21:24:53 +0300

    Add on date picked listener

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/newday/NewDayFragment.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/newday/NewDayFragment.kt
@@ -1,15 +1,11 @@
 package dev.kirillzhelt.maymaymay.newday
 
-
-import android.app.DatePickerDialog
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.DatePicker
 import androidx.fragment.app.viewModels
 import com.google.android.material.datepicker.MaterialDatePicker
 import dev.kirillzhelt.maymaymay.MainApplication
@@ -31,20 +27,19 @@ class NewDayFragment: Fragment() {
 
         val chooseDateButton: Button = inflatedView.findViewById(R.id.fragment_new_day_choose_date_btn)
 
-        chooseDateButton.setOnClickListener { view ->
-            showDatePickerDialog(view)
-        }
+        chooseDateButton.setOnClickListener(this::showDatePickerDialog)
 
         return inflatedView
     }
 
     private fun showDatePickerDialog(view: View) {
         val datePickerBuilder: MaterialDatePicker.Builder<Long> = MaterialDatePicker.Builder.datePicker()
-        datePickerBuilder.setTitleText("Hello world")
+        datePickerBuilder.setTitleText(R.string.date_picker_title)
 
         val picker = datePickerBuilder.build()
-        picker.show(fragmentManager!!, "datePicker")
+        picker.addOnPositiveButtonClickListener(newDayViewModel::onDatePicked)
 
+        picker.show(fragmentManager!!, "datePicker")
     }
 
 
--- app/app/src/main/java/dev/kirillzhelt/maymaymay/newday/NewDayViewModel.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/newday/NewDayViewModel.kt
@@ -1,10 +1,22 @@
 package dev.kirillzhelt.maymaymay.newday
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import dev.kirillzhelt.maymaymay.daysmodel.DaysRepository
+import java.util.*
 
 class NewDayViewModel(private val daysRepository: DaysRepository): ViewModel() {
 
+    private val _pickedDate = MutableLiveData<Date>(Date(System.currentTimeMillis()))
 
+    val pickedDate: LiveData<Date> = _pickedDate
+
+    fun onDatePicked(selection: Long) {
+        _pickedDate.value = Date(selection)
+
+        Log.i("Date picked", _pickedDate.value.toString())
+    }
 
 }
