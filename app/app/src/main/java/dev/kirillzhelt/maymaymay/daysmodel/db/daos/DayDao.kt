cb9064e0253c2994158557c73d050e94de166d01
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-07 18:02:27 +0300

    Create DayDao

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/db/daos/DayDao.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/db/daos/DayDao.kt
@@ -1,10 +1,31 @@
 package dev.kirillzhelt.maymaymay.daysmodel.db.daos
 
-import androidx.room.Dao
+import androidx.room.*
+import dev.kirillzhelt.maymaymay.daysmodel.db.entities.DayEntity
+import java.util.*
 
 @Dao
 interface DayDao {
 
+    @Insert
+    fun insert(day: DayEntity)
 
+    @Query("""
+        SELECT *
+        FROM days
+    """)
+    fun getDays(): List<DayEntity>
+
+    @Update
+    fun update(day: DayEntity)
+
+    @Delete
+    fun delete(day: DayEntity)
+
+    @Query("""
+        SELECT days.day_date
+        FROM days
+    """)
+    fun getDates(): List<Date>
 
 }
