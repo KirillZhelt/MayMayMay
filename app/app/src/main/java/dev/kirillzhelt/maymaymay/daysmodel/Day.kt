d20f491188c7d901c796da62c0ca507a40f886d0
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-07 17:09:23 +0300

    Convert Day data class to Room entity

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/Day.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/Day.kt
@@ -1,11 +1,26 @@
 package dev.kirillzhelt.maymaymay.daysmodel
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 // https://developer.android.com/training/data-storage/room
 // https://developer.android.com/training/data-storage/room/referencing-data
 
-data class Day(val date: Date,
-               var description: String,
-               val tags: MutableList<String>,
-               val grade: DayGrade)
+@Entity(tableName = "day_table")
+data class Day(
+    @PrimaryKey
+    @ColumnInfo(name = "day_date")
+    val date: Date,
+
+    @ColumnInfo(name = "description")
+    var description: String,
+
+    @ColumnInfo(name = "tags")
+    val tags: MutableList<String>,
+
+    @ColumnInfo(name = "grade")
+    val grade: DayGrade
+
+)
