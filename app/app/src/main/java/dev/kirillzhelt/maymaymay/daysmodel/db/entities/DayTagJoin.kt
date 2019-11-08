5200d5b8a70b662be0a150c7af15b8256572e119
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-08 17:56:24 +0300

    Set on delete cascade for DayTagJoin

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/db/entities/DayTagJoin.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/db/entities/DayTagJoin.kt
@@ -3,17 +3,20 @@ package dev.kirillzhelt.maymaymay.daysmodel.db.entities
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.ForeignKey.CASCADE
 
 @Entity(tableName = "day_tag_join",
     primaryKeys = ["day_id", "tag_id"],
     foreignKeys = [
         ForeignKey(entity = DayEntity::class,
             parentColumns = ["id"],
-            childColumns = ["day_id"]
+            childColumns = ["day_id"],
+            onDelete = CASCADE
             ),
         ForeignKey(entity = TagEntity::class,
             parentColumns = ["id"],
-            childColumns = ["tag_id"]
+            childColumns = ["tag_id"],
+            onDelete = CASCADE
             )
     ]
     )
