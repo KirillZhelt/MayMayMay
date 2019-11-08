a93ed4dcca9b04bbfcb6871f9c641b2fc875ee76
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-08 12:59:35 +0300

    Add TypeConverters for DayGrade

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/db/utils/Converters.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/daysmodel/db/utils/Converters.kt
@@ -1,6 +1,7 @@
 package dev.kirillzhelt.maymaymay.daysmodel.db.utils
 
 import androidx.room.TypeConverter
+import dev.kirillzhelt.maymaymay.daysmodel.DayGrade
 import java.util.*
 
 class Converters {
@@ -12,6 +13,16 @@ class Converters {
     @TypeConverter
     fun timestampFromDate(date: Date?): Long? {
         return date?.time
+    }
+
+    @TypeConverter
+    fun fromInt(value: Int?): DayGrade? {
+        return if (value != null) DayGrade.fromInt(value) else null
+    }
+
+    @TypeConverter
+    fun intFromGrade(grade: DayGrade?): Int? {
+        return grade?.grade
     }
 
 }
