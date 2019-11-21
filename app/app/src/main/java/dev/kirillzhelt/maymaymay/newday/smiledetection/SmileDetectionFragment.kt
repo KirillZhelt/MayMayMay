a182460d689030dbd101125c876c51a004a7cb7c
Kirill Zheltovski <kirillzhelt@gmail.com>
2019-11-21 16:31:45 +0300

    Bind Preview use case to CameraX

--- app/app/src/main/java/dev/kirillzhelt/maymaymay/newday/smiledetection/SmileDetectionFragment.kt
+++ app/app/src/main/java/dev/kirillzhelt/maymaymay/newday/smiledetection/SmileDetectionFragment.kt
@@ -1,26 +1,100 @@
 package dev.kirillzhelt.maymaymay.newday.smiledetection
 
 
+import android.Manifest
+import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
+import android.graphics.Matrix
 import android.os.Bundle
+import android.util.Size
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
+import androidx.camera.core.CameraX
+import androidx.camera.core.Preview
+import androidx.camera.core.PreviewConfig
+import androidx.core.content.ContextCompat
 
 import dev.kirillzhelt.maymaymay.R
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 /**
  * A simple [Fragment] subclass.
  */
 class SmileDetectionFragment : Fragment() {
 
+    private lateinit var viewFinder: TextureView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_smile_detection, container, false)
+        val inflatedView = inflater.inflate(R.layout.fragment_smile_detection, container, false)
+
+        viewFinder = inflatedView.findViewById(R.id.fragment_smile_detection_preview_txtr_v)
+
+        if (allPermissionsGranted()) {
+            viewFinder.post { startCamera() }
+        } else {
+            requestPermissions(REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        return inflatedView
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(requireContext(),
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        activity?.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_FULL_SENSOR
+    }
+
+    private fun startCamera() {
+        val previewConfig = PreviewConfig.Builder()
+            .setTargetResolution(Size(viewFinder.height, viewFinder.width))
+            .setLensFacing(CameraX.LensFacing.FRONT)
+            .build()
+
+        val preview = Preview(previewConfig)
+        preview.setOnPreviewOutputUpdateListener {
+            val parent = viewFinder.parent as ViewGroup
+            parent.removeView(viewFinder)
+            parent.addView(viewFinder, 0)
+
+            viewFinder.surfaceTexture = it.surfaceTexture
+        }
+
+        CameraX.bindToLifecycle(this, preview)
+
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        return REQUIRED_PERMISSIONS.all { ContextCompat.checkSelfPermission(requireContext(), it) ==
+                PackageManager.PERMISSION_GRANTED }
+    }
 }
